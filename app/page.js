b417ab9ee509ca3c15b856cb9b817bd6ac68f8ed
goutam <goutamr362@gmail.com>
2024-06-12 01:02:04 +0600

    Next-js install and setup

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+        <h2 className="text-center font-medium"> Hello Next js </h2>
+
+   </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
